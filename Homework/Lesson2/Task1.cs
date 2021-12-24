6ff66518423fbe984b1e7c8722625fea04819625
juliaosh <juliaaikasheva@gmail.com>
2021-12-24 03:53:53 +0300

    Lesson2 Task1

---
+++ Homework/Lesson2/Task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace HomeWork
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minimum day temperature: ");
+            double minT = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Maximum day temperature: ");
+            double maxT = Convert.ToDouble(Console.ReadLine());
+
+            double avTemp = (minT + maxT) / 2;
+            Console.WriteLine($"Average temperature: {avTemp}");
+        }
+
+
+    }
+}
