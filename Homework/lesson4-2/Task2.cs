a18150085e09ca7e4454056cb1ddbcb8b4824dfb
juliaosh <juliaaikasheva@gmail.com>
2022-01-09 06:11:06 +0300

    Lesson4

---
+++ Homework/lesson4-2/Task2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson4_2
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers separated by space");
+            string numbers = Console.ReadLine();
+            Console.WriteLine(CalculateSumm(numbers));
+
+            static double CalculateSumm(string numbers)
+            {
+                string temp = "";
+                double summ = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] != ' ')
+                    {
+                        temp += numbers[i];
+                    }
+                    else
+                    {
+                        double tempDouble = Convert.ToDouble(temp);
+                        summ += tempDouble;
+                        temp = "";
+                    }
+                }
+                return summ + Convert.ToDouble(temp);
+            }
+        }
+    }
+}
